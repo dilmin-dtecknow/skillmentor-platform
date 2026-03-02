fecb09b230e64405b25e329da2c4295bd8506c9e
dilmin-dtecknow <fernandodoloman42@gmail.com>
2026-03-02 20:20:43 +0530

    backend initialized

---
+++ backend/skill-mentor-backend/src/main/java/com/dilmin/skill_mentor_backend/SkillMentorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dilmin.skill_mentor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillMentorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillMentorBackendApplication.class, args);
+	}
+
+}
